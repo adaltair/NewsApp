af36e501c4c05767884e89abd5e0dec6e0e09afb
Altair Adebietov <83090396+adaltair@users.noreply.github.com>
2022-12-12 01:04:49 +0600

    wordmodel

--- app/src/main/java/com/example/newsapp/Word.kt
+++ app/src/main/java/com/example/newsapp/Word.kt
@@ -6,9 +6,4 @@ import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "word_table")
-class Word(
-
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "word") val word: String
-
-)
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
